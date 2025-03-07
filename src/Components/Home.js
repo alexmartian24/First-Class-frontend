7977a752a38cbb7b37b3555f274bdec56c2695cb
nyusporttkd <139005055+hjk665@users.noreply.github.com>
2025-03-06 19:35:53 -0500

    Update home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,6 +7,16 @@ function Home() {
     <div className="home-container">
       <div className="content-center">
         <h1>Journal</h1>
+        <p>
+          Welcome to Journal, a platform where you can manage and explore
+          various submissions, people, and data records seamlessly. Whether
+          you&apos;re here to contribute, review, or browse, this tool is designed
+          to make your experience efficient and intuitive.
+        </p>
+        <p>
+          Click below to get started and explore the people database, manage
+          submissions, and interact with the system.
+        </p>
         <Link to="/people" className="btn-glitch-fill">
           <span className="text">Get Started</span>
           <span className="text-decoration"> _</span>
